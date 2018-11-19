d08059e2ef80765e80324ed64f7e99642e545ca5
Don Ward <10is0420@gmail.com>
2018-11-19 02:02:28 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
-
+def unsafe?(answer)
+  if answer < 40 || answer > 60
+    return true
+  else answer
+    return false
+end
 end
 
 
-
-def not_safe?(speed)
-	
+def not_safe?(answer)
+	return (answer < 40 || answer > 60 ) ? true : false
 end
+
 	
 
 
