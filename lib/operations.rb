2c1d6d22c0e35277308c9438d03ad2b323b4da7b
Don Ward <10is0420@gmail.com>
2018-11-19 01:54:37 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(answer)
   if answer < 40 || answer > 60
-    return true
-  else answer
-    return false
 end
 
 
+def not_safe?(answer)
+	return (answer < 40 || speed > 60 ) ? true : false
+end
 
 	
 
